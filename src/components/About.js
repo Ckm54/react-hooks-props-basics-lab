259b96aed501bf1b14f11233ad0b957c40e18128
Collins <mutaic84@gmail.com>
2022-04-28 22:52:35 +0300

    display paragraph conditionally

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from "./Links"
 
 function About(props) {
+
+  function displayParagraph() {
+    if(!props.bio || props.bio === ""){
+      return;
+    }
+    return <p>{props.bio}</p>
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {displayParagraph()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
